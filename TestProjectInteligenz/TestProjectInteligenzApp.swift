5a6762b8773d97d52f208e4d319ed49adf32c8eb
José Ignacio <jose.ignacio.juan@ciberexperis.es>
2022-03-05 18:04:51 +0100

    Adds project

---
+++ TestProjectInteligenz/TestProjectInteligenzApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectInteligenzApp.swift
+//  TestProjectInteligenz
+//
+//  Created by Jose Ignacio de Juan Díaz on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectInteligenzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
